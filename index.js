4e8bf63b09c6c56421be247ac8c25c07bbb39065
NeeKin <ubuntu@DESKTOP-NeeKin.localdomain>
2022-09-14 07:49:13 +0800

    拆分配置项

--- index.js
+++ index.js
@@ -7,9 +7,14 @@ const Vision = require("@hapi/vision");
 const adminPlugin = require("./lib/admin");
 const jwtPlugin = require("./lib/jwt");
 const swaggerPlugin = require("./lib/swagger");
+
 exports.plugin = {
   pkg,
   register: async function (server, options) {
+    const  registerJwtPlugin ={
+      plugin: jwtPlugin,
+      options:{}
+    }
     if (options.admin) {
       let adminOptions = _.merge(adminPlugin.options, options.admin);
       adminPlugin.options = adminOptions;
@@ -18,13 +23,17 @@ exports.plugin = {
       let swaagerOptions = _.merge(swaggerPlugin.options, options.swaager);
       swaggerPlugin.options = swaagerOptions;
     }
+    if(options.jwt){
+      let jwtOptions = _.merge(registerJwtPlugin.options, options.jwt);
+      registerJwtPlugin.options = jwtOptions
+    }
 
     await server.register([
       Inert,
       Vision,
       adminPlugin,
       swaggerPlugin,
-      { plugin: jwtPlugin },
+      registerJwtPlugin,
     ]);
   },
 };
