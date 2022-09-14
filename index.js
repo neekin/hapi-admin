66fc0396972fcb3a16efa31b82235a3fff9c08b7
NeeKin <7124014+neekin@users.noreply.github.com>
2022-09-14 03:50:57 +0000

    update c代码组织方式

--- index.js
+++ index.js
@@ -1,39 +1,14 @@
-"use strict";
-const pkg = require("./package");
-const _ = require("lodash");
-const Inert = require("@hapi/inert");
-const Vision = require("@hapi/vision");
-// 引入依赖的插件
-const adminPlugin = require("./lib/admin");
-const jwtPlugin = require("./lib/jwt");
-const swaggerPlugin = require("./lib/swagger");
-
-exports.plugin = {
-  pkg,
-  register: async function (server, options) {
-    const  registerJwtPlugin ={
-      plugin: jwtPlugin,
-      options:{}
+const Hapi = require('@hapi/hapi')
+const _ = require('lodash')
+module.exports ={
+    createServer:async function(options){
+        const initOptions={
+            port:3000,
+            host:"localhost"
+        }
+        const serverOptions=_.merge(initOptions,options.server)
+        const server = Hapi.server(serverOptions)
+        await server.register({plugin:require('./plugins'),options:{...options}})
+        return server
     }
-    if (options.admin) {
-      let adminOptions = _.merge(adminPlugin.options, options.admin);
-      adminPlugin.options = adminOptions;
-    }
-    if(options.swaager){
-      let swaagerOptions = _.merge(swaggerPlugin.options, options.swaager);
-      swaggerPlugin.options = swaagerOptions;
-    }
-    if(options.jwt){
-      let jwtOptions = _.merge(registerJwtPlugin.options, options.jwt);
-      registerJwtPlugin.options = jwtOptions
-    }
-
-    await server.register([
-      Inert,
-      Vision,
-      adminPlugin,
-      swaggerPlugin,
-      registerJwtPlugin,
-    ]);
-  },
-};
+} 
---
+++ plugins.js
@@ -0,0 +1,39 @@
+"use strict";
+const pkg = require("./package");
+const _ = require("lodash");
+const Inert = require("@hapi/inert");
+const Vision = require("@hapi/vision");
+// 引入依赖的插件
+const adminPlugin = require("./lib/admin");
+const jwtPlugin = require("./lib/jwt");
+const swaggerPlugin = require("./lib/swagger");
+
+exports.plugin = {
+  pkg,
+  register: async function (server, options) {
+    const  registerJwtPlugin ={
+      plugin: jwtPlugin,
+      options:{}
+    }
+    if (options.admin) {
+      let adminOptions = _.merge(adminPlugin.options, options.admin);
+      adminPlugin.options = adminOptions;
+    }
+    if(options.swaager){
+      let swaagerOptions = _.merge(swaggerPlugin.options, options.swaager);
+      swaggerPlugin.options = swaagerOptions;
+    }
+    if(options.jwt){
+      let jwtOptions = _.merge(registerJwtPlugin.options, options.jwt);
+      registerJwtPlugin.options = jwtOptions
+    }
+
+    await server.register([
+      Inert,
+      Vision,
+      adminPlugin,
+      swaggerPlugin,
+      registerJwtPlugin,
+    ]);
+  },
+};
