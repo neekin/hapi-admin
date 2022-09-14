b7c426db685f676b3eda587337ab795d883b5a79
NeeKin <ubuntu@DESKTOP-NeeKin.localdomain>
2022-09-14 20:00:42 +0800

    fix options bugs

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Hapi = require('@hapi/hapi')
 const _ = require('lodash')
 module.exports ={
-    createServer:async function(options){
+    createServer:async function(options={}){
         const initOptions={
             port:3000,
             host:"localhost"
