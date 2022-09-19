35bfd68694c405b1028e49a5939164aa41aa74aa
ubuntu <ubuntu@localhost.localdomain>
2022-09-19 14:22:06 +0800

    更新命令行工具

---
+++ test/cli.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+const { program } = require('commander')
+const package = require('../package.json')
+const registerCommand = require('../lib/command')
+registerCommand(program)
+
+
+program.name('hapi-admin')
+    .description('hapi-admin 一些生成代码的命令')
+    .version(package.version);
+
+program.parse(process.argv)
