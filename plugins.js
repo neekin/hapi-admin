30fd5a6a302fa1c5bd85043324541fc85304a419
ubuntu <ubuntu@localhost.localdomain>
2022-09-21 06:44:11 +0800

    清理代码

--- plugins.js
+++ plugins.js
@@ -1,18 +1,17 @@
 "use strict";
 const pkg = require("./package");
 const _ = require("lodash");
-const fs = require('fs')
-const path = require('path')
+
 const Inert = require("@hapi/inert");
 const Vision = require("@hapi/vision");
+
 // 引入依赖的插件
-// const adminPlugin = require("./lib/plugins/admin")
 const jwtPlugin = require("./lib/plugins/jwt");
 const swaggerPlugin = require("./lib/plugins/swagger");
-// const resources = require('./lib/plugins/registerResouces')
+
 const routes = require("./lib/plugins/registerRoutes");
 const next = require("./lib/plugins/next");
-const checkPro = require("./utils/check_pro");
+const { checkPro ,checkSimple } = require("./utils/check_pro");
 exports.plugin = {
   pkg,
   register: async function (server, options) {
@@ -28,36 +27,6 @@ exports.plugin = {
       let nextOptions = _.merge(nextPlugin.options, options.next);
       nextPlugin.options = nextOptions;
     }
-    if (checkPro()) {
-      // console.log("引入了 hapi-admin-pro");
-      // const file = path.join(path.resolve('./node_modules/hapi-admin-pro'),'pages')
-      // const taget = path.join(path.resolve('./node_modules/hapi-admin'),'pages/admin')
-      // // console.log(file)
-      // fs.symlink(file,taget,(err)=>{})
-    }
-    //  else {
-    //   if (options.admin) {
-    //     let adminOptions = _.merge(adminPlugin.options, options.admin);
-    //     adminPlugin.options = adminOptions;
-    //   }
-    //   if (!adminPlugin.options.resources) {
-    //     adminPlugin.options.resources = resources;
-    //   } else {
-    //     adminPlugin.options.resources = _.merge(
-    //       resources,
-    //       adminPlugin.options.resources
-    //     );
-    //   }
-    // }
-    // if (options.admin) {
-    //   let adminOptions = _.merge(adminPlugin.options, options.admin);
-    //   adminPlugin.options = adminOptions;
-    // }
-    // if(!adminPlugin.options.resources){
-    //   adminPlugin.options.resources=resources
-    // }else{
-    //   adminPlugin.options.resources = _.merge(resources,adminPlugin.options.resources)
-    // }
     if (options.swagger) {
       let swaggerOptions = _.merge(swaggerPlugin.options, options.swagger);
       swaggerPlugin.options = swaggerOptions;
@@ -66,15 +35,22 @@ exports.plugin = {
       let jwtOptions = _.merge(registerJwtPlugin.options, options.jwt);
       registerJwtPlugin.options = jwtOptions;
     }
-
+    if(checkSimple() && !checkPro()){
+      console.log('简单管理界面组件')
+      await server.register({
+       plugin:   require('hapi-admin-simple'),
+       options
+      })
+    }
     await server.register([
       Inert,
       Vision,
-      // adminPlugin,
       swaggerPlugin,
       registerJwtPlugin,
       nextPlugin,
-      ...routes,
+      ...routes
     ]);
+
+
   },
 };
--- utils/check_pro.js
+++ utils/check_pro.js
@@ -1,8 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = ()=>{
-    const file = path.resolve('./node_modules/hapi-admin-pro')
-    return fs.existsSync(file)
+module.exports ={
+    checkSimple(){
+        const file = path.resolve('./node_modules/hapi-admin-simple')
+        return fs.existsSync(file)
+    },
+    checkPro(){
+        const file = path.resolve('./node_modules/hapi-admin-pro')
+        return fs.existsSync(file)
+    }
 }
-// fs.existsSync(__dirname,'node_modules')
