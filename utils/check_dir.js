d35cc22d059e1dd1c20a1c0b1027c61fff8a5f87
ubuntu <ubuntu@localhost.localdomain>
2022-09-22 01:01:53 +0800

    remove console

--- utils/check_dir.js
+++ utils/check_dir.js
@@ -3,10 +3,8 @@ const fs = require('fs-extra');
 
 module.exports = {
   createDir(dir) {
-    console.log(44444444,dir)
     if (!fs.existsSync(dir)) {
-      console.log('33333333')
-      fs.mkdirSync(dir);
+      fs.mkdirSync(dir)
     }
   },
   checkDir(dir){
